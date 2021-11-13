16a46261606fdbbc282985393c370558b1a28885
Wenting Zhao <88762053+Wentinggg@users.noreply.github.com>
2021-11-13 01:10:46 -0800

    Update database_endpoint.py

--- database_endpoint.py
+++ database_endpoint.py
@@ -39,7 +39,7 @@ def shutdown_session(response_or_exc):
 def log_message(d):
     # Takes input dictionary d and writes it to the Log table
     for key in d.keys():
-        add_order = Log(message=json.dumps(d[key]))
+        add_order = Log(message=json.dumps(d[str(key)]))
         g.session.add(add_order)
     g.session.commit()
 
